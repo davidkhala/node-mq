43cc8be2e4299b7eadae439d335b4189a9f02cd1
DavidLiu <david.yx.liu@oracle.com>
2021-04-10 09:56:41 -0400

    [WIP] kafka.yml

---
+++ kafka/index.js
@@ -0,0 +1,55 @@
+const {Kafka} = require('kafkajs')
+
+
+class KafkaManager {
+
+    /**
+     *
+     * @param {string[]} brokers
+     * @param {string} username
+     * @param {string} password
+     * @param clientId
+     */
+    constructor(brokers, {username, password, clientId} = {}) {
+        const config = {
+            clientId,
+            brokers,
+        }
+        if (username) {
+            Object.assign(config, {
+                ssl: true,
+                sasl: {
+                    mechanism: 'plain', // scram-sha-256 or scram-sha-512
+                    username,
+                    password
+                },
+            })
+        }
+        this.kafkaManager = new Kafka(config)
+        this._admin = this.admin();
+    }
+
+    admin() {
+        return this.kafkaManager.admin()
+    }
+
+    async connect() {
+        await this._admin.connect()
+    }
+
+    async disconnect() {
+        await this._admin.disconnect()
+    }
+
+    async listTopics() {
+        return this._admin.listTopics()
+    }
+
+    producer() {
+        return kafka.producer()
+    }
+
+
+}
+
+module.exports = KafkaManager
---
+++ kafka/test/IBM.js
@@ -0,0 +1,28 @@
+
+const kafka_brokers_sasl = [
+    "broker-3-gry0n7cv2tblgg8t.kafka.svc08.us-south.eventstreams.cloud.ibm.com:9093",
+    "broker-4-gry0n7cv2tblgg8t.kafka.svc08.us-south.eventstreams.cloud.ibm.com:9093",
+    "broker-5-gry0n7cv2tblgg8t.kafka.svc08.us-south.eventstreams.cloud.ibm.com:9093",
+    "broker-0-gry0n7cv2tblgg8t.kafka.svc08.us-south.eventstreams.cloud.ibm.com:9093",
+    "broker-2-gry0n7cv2tblgg8t.kafka.svc08.us-south.eventstreams.cloud.ibm.com:9093",
+    "broker-1-gry0n7cv2tblgg8t.kafka.svc08.us-south.eventstreams.cloud.ibm.com:9093"
+]
+const username = 'token'
+const KafkaManager = require('../index')
+const topic = 'davidkhala-node-mq'
+const assert = require('assert')
+describe('IBM Event Stream', function () {
+    this.timeout(30000)
+    const {api_key} = process.env
+    if (!api_key) {
+        throw Error('Missing api_key')
+    }
+    it('connect', async () => {
+        const client = new KafkaManager(kafka_brokers_sasl, {username, password: api_key})
+
+        await client.connect()
+        const result = await client.listTopics()
+        assert.strictEqual(result[0], topic)
+        await client.disconnect()
+    })
+})
