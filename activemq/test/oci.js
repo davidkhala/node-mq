7cc561a8ce7191fe3051cb22c7afc1691efc04fa
David Liu <david-khala@hotmail.com>
2023-01-31 15:00:06 +0800

    [WIP] connect to OCI [Test] migrate activemq provision to davidkhala/mq

--- activemq/test/oci.js
+++ activemq/test/oci.js
@@ -3,7 +3,7 @@ import {STOMPClient} from '../stomp.js'
 describe('raw tcp', function () {
     this.timeout(0)
 
-    it('connect', async () => {
+    it.skip('connect', async () => {
 
         const destination = '/queue/someQueueName';
         const user = 'oracleidentitycloudservice/david.yx.liu@oracle.com'
