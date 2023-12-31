4fcd4fb9cf9c143ab7bc8626bd5bb6e5755251aa
David Liu <david-khala@hotmail.com>
2023-12-31 20:20:56 +0800

    add localhost support

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # node-mq
 nodejs wheels for popular message queues
-
-## Reference
-- [IBM: Message queue vs. …](https://www.ibm.com/cloud/learn/message-queues#toc-message-qu-MSaazifj)
--- kafka/test/localhost.js
+++ kafka/test/localhost.js
@@ -4,14 +4,13 @@ import assert from 'assert'
 describe('docker', function () {
     this.timeout(0)
     it('connect', async () => {
-        const kafka_brokers_sasl = ['kafka1:9092',
-            'kafka2:9092','kafka3:9092']
+        const kafka_brokers_sasl = ['localhost:9094']//  ['kafka1:9092',            'kafka2:9092','kafka3:9092']
         const client = new KafkaManager(kafka_brokers_sasl)
 
         await client.connect()
         const topics = await client.listTopics()
 
-        assert.ok(topics.includes('streaming'))
+        assert.ok(topics.length ===0)
         await client.disconnect()
     })
 })
