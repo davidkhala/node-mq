81e77074cc555f60b79a77cc8a8344763ab1029e
David Liu <david-khala@hotmail.com>
2024-01-06 07:00:25 +0800

    align localhost port

--- kafka/test/localhost.js
+++ kafka/test/localhost.js
@@ -4,7 +4,7 @@ import assert from 'assert'
 describe('docker', function () {
     this.timeout(0)
     it('connect', async () => {
-        const kafka_brokers_sasl = ['localhost:9094']//  ['kafka1:9092',            'kafka2:9092','kafka3:9092']
+        const kafka_brokers_sasl = ['localhost:9092']//  ['kafka1:9092',            'kafka2:9092','kafka3:9092']
         const client = new KafkaManager(kafka_brokers_sasl)
 
         await client.connect()
