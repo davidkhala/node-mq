3bd44696eeacc8c6b2834390f878a538bf50b7d6
David Liu <david-khala@hotmail.com>
2024-01-07 21:49:49 +0800

    [WIP] @davidkhala/rabbitmq

--- activemq/test/localhost.js
+++ activemq/test/localhost.js
@@ -1,31 +1,30 @@
 import {WebsocketClient} from '../websocketClient.js';
-import {STOMPClient} from '../stomp.js'
-import {sleep} from '@davidkhala/light/index.js'
+import {STOMPClient} from '../stomp.js';
 
 describe('STOMP', function () {
-    this.timeout(0)
-    it('connect', async () => {
-        const host = 'localhost';
-        const client = new STOMPClient(host)
-        await client.connect()
+	this.timeout(0);
+	it('connect', async () => {
+		const host = 'localhost';
+		const client = new STOMPClient(host);
+		await client.connect();
 
-        client.send('a', 'b')
-        await client.disconnect()
-    })
+		client.send('a', 'b');
+		await client.disconnect();
+	});
 
-})
+});
 
 describe('websocket', function () {
-    this.timeout(0)
-    const host = 'localhost';
-    const conn = new WebsocketClient(host);
+	this.timeout(0);
+	const host = 'localhost';
+	const conn = new WebsocketClient(host);
 
-    it('connect', async () => {
-        await conn.connect()
+	it('connect', async () => {
+		await conn.connect();
 
-        conn.send('a', 'b')
-        await conn.disconnect()
+		conn.send('a', 'b');
+		await conn.disconnect();
 
-    })
-})
+	});
+});
 
