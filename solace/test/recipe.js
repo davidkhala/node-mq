24af163dd835dd3964f63119738688f37a416a08
David Liu <david-khala@hotmail.com>
2024-01-08 05:20:05 +0800

    [WIP] @davidkhala/pulsar

--- solace/test/recipe.js
+++ solace/test/recipe.js
@@ -13,8 +13,8 @@ export async function docker(manager, {websocket = 8008, portal = 8080, username
 
 	opts.setPortBind(`${websocket}:8008`);
 	opts.setPortBind(`${portal}:8080`);
-	opts.setName('solace');
-	opts.setEnv(env);
+	opts.name = 'solace';
+	opts.env = env;
 	opts.opts.HostConfig.ShmSize = Size['2GB'];
 
 	await manager.containerStart(opts.opts, undefined, true);
