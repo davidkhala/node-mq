d24ac63bb64847c6d54c9c8338f791d62bee5c5c
David Liu <david-khala@hotmail.com>
2025-04-25 18:56:08 +0800

    WIP @testcontainers/kafka

--- kafka/index.js
+++ kafka/index.js
@@ -1,72 +1,79 @@
 import kafkajs from 'kafkajs';
 import PubSub, {Admin as AbstractAdmin} from '@davidkhala/pubsub';
+
 const {Admin, Kafka} = kafkajs;
 
 export default class KafkaManager extends PubSub {
 
-	/**
-	 *
-	 * @param {string[]} brokers
-	 * @param [ssl] default to true
-	 * @param {string} [clientId] default to "kafkajs"
-	 * @param sasl sasl config
-	 * @param [logger]
-	 */
-	constructor(brokers, {ssl=true, clientId='kafkajs', sasl} = {}, logger) {
-		super({
-			name: clientId,
-			username:sasl?sasl.username:undefined,
-			password:sasl?sasl.password:undefined,
-		}, undefined, logger);
-		const config = {
-			clientId,
-			brokers,
-			ssl,
-			sasl
-		};
+    /**
+     *
+     * @param {string[]} brokers
+     * @param [ssl] default to true
+     * @param {string} [clientId] default to "kafkajs"
+     * @param sasl sasl config
+     * @param [logger]
+     */
+    constructor(brokers, {ssl = true, clientId = 'kafkajs', sasl} = {}) {
+        super();
+        this.name = clientId
+        if (sasl) {
+            const {username, password} = sasl
+            this.username = username
+            this.password = password
+        }
 
-		this.kafkaManager = new Kafka(config);
-	}
+        this.kafkaManager = new Kafka({
+            clientId,
+            brokers,
+            ssl,
+            sasl
+        });
+    }
 
-	get admin() {
-		if (!this._admin) {
-			this._admin = new KafkaAdmin(this.kafkaManager.admin());
-		}
-		return this._admin;
-	}
+    get dba() {
+        if (!this._admin) {
+            this._admin = new KafkaAdmin(this.kafkaManager.admin());
+        }
+        return this._admin;
+    }
 
 
-	get producer() {
-		return this.kafkaManager.producer();
-	}
+    get producer() {
+        return this.kafkaManager.producer();
+    }
 
-	get consumer() {
-		return this.kafkaManager.consumer();
-	}
+    get consumer() {
+        return this.kafkaManager.consumer();
+    }
 
 }
 
 export class KafkaAdmin extends AbstractAdmin {
-	/**
-	 *
-	 * @param {Admin} admin
-	 */
-	constructor(admin) {
-		super();
-		this.admin = admin;
-	}
+    /**
+     *
+     * @param {Admin} admin
+     */
+    constructor(admin) {
+        super();
+        this.admin = admin;
+    }
 
-	async listTopics() {
-		return this.admin.listTopics();
-	}
+    async listTopics() {
+        return this.admin.listTopics();
+    }
 
-	async _connect() {
-		await this.admin.connect();
-		return true;
-	}
+    async _connect() {
+        await this.admin.connect();
+        return true;
+    }
 
-	async disconnect() {
-		await this.admin.disconnect();
-	}
+    async disconnect() {
+        await this.admin.disconnect();
+    }
 
+    async clear() {
+        const topics = await this.listTopics()
+
+        await this.admin.deleteTopics({topics})
+    }
 }
--- kafka/test/localhost.js
+++ kafka/test/localhost.js
@@ -1,16 +1,32 @@
 import assert from 'assert';
 import Insecure from '../insecure.js';
+import {KafkaContainer} from "@testcontainers/kafka";
 
 describe('docker', function () {
-	this.timeout(0);
-	it('connect', async () => {
-		const kafka_brokers_sasl = ['localhost:9092'];//  ['kafka1:9092',            'kafka2:9092','kafka3:9092']
-		const client = new Insecure(kafka_brokers_sasl);
-		const {admin} = client;
-		await admin.connect();
-		const topics = await admin.listTopics();
+    this.timeout(0);
+    it('connect', async () => {
+        const kafka_brokers_sasl = ['localhost:9092'];//  ['kafka1:9092',            'kafka2:9092','kafka3:9092']
+        const client = new Insecure(kafka_brokers_sasl);
+        const {admin} = client;
+        await admin.connect();
+        const topics = await admin.listTopics();
 
-		assert.ok(topics.length === 0);
-		await admin.disconnect();
-	});
+        assert.ok(topics.length === 0);
+        await admin.disconnect();
+    });
+});
+describe('testcontainers', function () {
+    this.timeout(0);
+    let controller
+    before(async () => {
+        const container = new KafkaContainer().withKraft().withExposedPorts(9093)
+        controller = await container.start();
+    })
+    it('connect', async () => {
+
+    })
+    after(async () => {
+        await controller.stop()
+    })
+
 });
